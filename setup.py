b134605c9cbf70215a83b394847fd05ced191dc3
Michal <michal@maithu.com>
2016-07-15 11:01:56 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-cookie-policy',
+    version='0.1',
+    packages=['cookie_policy'],
+    url='',
+    license='',
+    author='dcosentino',
+    author_email='',
+    description=''
+)
